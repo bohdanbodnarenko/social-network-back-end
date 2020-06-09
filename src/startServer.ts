ce069245a77044697b6557ea067f5ffdddde2f34
Bogdan <bodya.bodnarenko@gmail.com>
2020-06-09 12:30:35 +0300

    removed useless logging

--- src/startServer.ts
+++ src/startServer.ts
@@ -4,7 +4,7 @@ import { Application } from 'express';
 
 import { redis } from './redis';
 import { app } from './app';
-import { createTypeormConn, logger } from './utils';
+import { createTypeormConn } from './utils';
 
 export const startServer = async (): Promise<Application> => {
     if (process.env.NODE_ENV === 'test') {
@@ -12,16 +12,9 @@ export const startServer = async (): Promise<Application> => {
     }
 
     const port = process.env.PORT || '4000';
-    const host = process.env.HOST || '0.0.0.0';
-
     await createTypeormConn();
 
-    app.listen(+port, host, e => {
-        if (e) {
-            logger.error(e);
-        }
-        logger.info(`Listening on ${host}:${port}`);
-    });
+    app.listen(port);
 
     return app;
 };
